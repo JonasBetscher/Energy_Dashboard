6201acaa770744e8a296b3721b28d67b9c92eb10
JonasBetscher <74207438+JonasBetscher@users.noreply.github.com>
2024-03-21 17:35:26 +0000

    Uploaded files

---
+++ Energy_Dashboard.py
@@ -0,0 +1,340 @@
+import dash
+from dash import html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+import pandas as pd
+from sklearn import metrics
+import numpy as np
+import seaborn as sb
+import matplotlib.pyplot as plt
+import io
+import base64
+import Energy_Dashboard_helpers as helpers
+import json
+import plotly.graph_objs as go
+
+# Load the training data
+training_data = helpers.load_data()
+
+# External stylesheets for the Dash app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# Reading data from a CSV file into a Pandas DataFrame
+df = pd.read_csv(r'testData_2019_Civil - testData_2019_Civil.csv')
+
+# Prepare 2019 data
+df_test = helpers.create_test_data(df)
+
+# Creating a Dash application instance
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
+
+# Function to generate an HTML table from a DataFrame
+def generate_table(dataframe, start_row=0, max_rows=10):
+    return html.Div([
+        html.Table([
+            html.Thead(
+                html.Tr([html.Th(col) for col in dataframe.columns])
+            ),
+            html.Tbody([
+                html.Tr([
+                    html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+                ]) for i in range(start_row, min(start_row + max_rows, len(dataframe)))
+            ])
+        ])
+    ])
+
+def generate_slider(dataframe):
+    return dcc.Slider(
+        id='row-slider',
+        min=0,
+        max=len(dataframe),
+        step=1,
+        value=0,
+        marks={i: dataframe.iloc[i]['Date'] for i in range(0, len(dataframe)+1, len(dataframe) // 10)}
+    )
+
+# Defining the layout of the Dash app
+app.layout = html.Div([
+    html.H2('Power Consumption Forecast Dashboard'),  # Main title
+    html.P('Learn how to forecast hourly power consumption using machine learning.'),  # Subtitle
+    dcc.Tabs(id='main-tabs', value='data-exploration', children=[  # Tabs for different sections
+        dcc.Tab(label='Data Exploration', value='data-exploration'),  # Data Exploration tab
+        dcc.Tab(label='Feature Exploration', value='feature-selection'),  # Feature Exploration tab
+        dcc.Tab(label='Forecasting', value='forecasting')  # Forecasting tab
+    ],
+    style={'fontSize': '20px'}),  # Set the font size of tab labels
+    html.Div(id='main-tabs-content')  # Container for tab content
+])
+
+# Data Exploration Tab
+data_exploration_tab = html.Div([
+    html.H5('Explore the data using tabular data or graphs'),  # Subtitle for the Data Exploration tab
+    dcc.Tabs(id='data-exploration-tabs', value='tab-1', children=[  # Tabs for different visualization types
+        dcc.Tab(label='Table', value='tab-1', id='data-table'),  # Table tab
+        dcc.Tab(label='Graph', value='tab-2'),  # Graph tab
+    ]),
+    html.Div(id='data-exploration-tabs-content') # Container for tab content
+])
+
+# Callback to render content based on the selected tab in Data Exploration section
+@app.callback(Output('data-exploration-tabs-content', 'children'), [Input('data-exploration-tabs', 'value')])
+def render_data_exploration_content(tab):
+    if tab == 'tab-1':  # If Table tab is selected
+        return html.Div([
+            generate_slider(df),  # Generating slider
+            html.Div(id='data-table')  # Container for the table
+        ])
+    elif tab == 'tab-2':  # If Graph tab is selected
+        return html.Div([
+            dcc.Dropdown(
+                id='plot-type-dropdown',
+                options=[
+                    {'label': 'Bar Chart', 'value': 'bar'},
+                    {'label': 'Line Chart', 'value': 'line'}
+                ],
+                value='bar',  # Default value for dropdown
+                clearable=False
+            ),
+            dcc.Dropdown(
+                id='data-dropdown',
+                options=[{'label': col, 'value': col} for col in df.columns],
+                value=[df.columns[1]],  # Default value for dropdown
+                multi=True  # Allow multiple selections
+            ),
+            dcc.Graph(id='yearly-data')  # Graph component for displaying data
+        ])
+    
+# Updating the callback to generate the table based on slider value
+@app.callback(
+    Output('data-table', 'children'),
+    [Input('row-slider', 'value')]
+)
+def update_table(start_row):
+    return generate_table(df, start_row=start_row)
+
+# Callback to render content based on the selected main tab
+@app.callback(Output('main-tabs-content', 'children'), [Input('main-tabs', 'value')])
+def render_main_tabs_content(tab):
+    if tab == 'data-exploration':
+        return data_exploration_tab  #Render Data Exploration tab content
+    elif tab == 'feature-selection':
+        return feature_selection_tab
+    elif tab == 'forecasting':
+        return forecasting_tab  # Render Forecasting tab content
+
+# Callback to update the graph based on selected plot type and data columns
+@app.callback(Output('yearly-data', 'figure'),
+              [Input('plot-type-dropdown', 'value'),
+               Input('data-dropdown', 'value')])
+def update_graph(plot_type, selected_data):
+    if not selected_data:
+        return {}  # Return empty graph if no data selected
+    
+    data = []
+    for col in selected_data:
+        data.append({'x': df['Date'], 'y': df[col], 'type': plot_type, 'name': f'{col} (kWh)'})
+    
+    return {
+        'data': data,
+        'layout': {
+            'title': 'IST Civil Building Weather and Power Consumption Data',
+        }
+    }
+
+
+# Feature Exploration
+# --------------------------------------------------------------------------------------------------------------------------------
+# Calculate correlation matrix
+correlation_matrix = training_data.corr()
+
+# Convert the correlation matrix to a base64 encoded image
+buffer = io.BytesIO()
+plt.figure(figsize=(10, 8))
+sb.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+plt.title('Correlation Matrix')
+plt.savefig(buffer, format='png')
+correlation_matrix_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+# get the random Forest metrics
+random_forest_results = helpers.calculate_feature_importance_RF(training_data)
+
+# Layout with the Feature Exploration Tab
+feature_selection_tab = html.Div([
+    html.H5('Learn about the forecasting relevance of the features.'),  # Subtitle for the Feature Exploration tab
+    html.P('Some features have been modified and some have been added. Analyse them by selecting the feature selection method.'),  # Explanation text
+    dcc.Dropdown(
+        id='feature-selection-method-dropdown',
+        options=[
+            {'label': 'Correlation Matrix', 'value': 'correlation'},
+            {'label': 'Random Forest', 'value': 'random_forest'}
+        ],
+        value='correlation',  # Default value
+        clearable=False
+    ),
+    html.Div(id='feature-selection-output')
+])
+
+# Callback for performing feature selection and displaying results
+@app.callback(
+    Output('feature-selection-output', 'children'),
+    Input('feature-selection-method-dropdown', 'value')
+)
+def perform_feature_selection(method):
+    if method == 'correlation':
+        return html.Div([
+        html.Img(src=f'data:image/png;base64,{correlation_matrix_image}',
+        style={'max-width': '100%', 'height': 'auto'})
+    ])
+    elif method == 'random_forest':
+        return html.Div([
+            html.H4('Random Forest Feature Selection Results:'),
+            html.Table([
+                html.Thead(
+                    html.Tr([html.Th(col) for col in random_forest_results.columns])
+                ),
+                html.Tbody([
+                    html.Tr([
+                        html.Td(random_forest_results.iloc[i][col]) for col in random_forest_results.columns
+                    ]) for i in range(min(len(random_forest_results), 10))  # Display first 10 rows
+                ])
+            ])
+        ])
+
+
+
+# Forecasting
+# --------------------------------------------------------------------------------------------------------------------------------
+# Define forecasting methods
+methods = {
+    'Linear Regression',
+    'Support Vector Machine (RBF kernel)',
+    'Multi-layer Perceptron',
+    'Decision Tree',
+    'Random Forest',
+    'Gradient Boosting',
+    'XGBoost',
+}
+
+# Define available metrics
+available_metrics = ['Mean Absolute Error (MAE)', 'Mean Bias Error (MBE)', 'Mean Squared Error (MSE)',
+                     'Root Mean Squared Error (RMSE)', 'Coefficient of Variation RMSE (cvRMSE)', 'Normalized Mean Bias Error (NMBE)']
+
+# Forecasting Tab
+forecasting_tab = html.Div([
+    html.H5('Let´s predict the power consumption for the next hour!'),  # Subtitle for the Forecasting tab
+    html.P('Train a model on 2017 and 2018 data and predict the 2019 power consumption based on the previous hour. Be patient ;)'),  # Explanation text
+    dcc.Dropdown(  # Dropdown for selecting forecasting method
+        id='forecasting-method-dropdown',
+        options=[{'label': method, 'value': method} for method in methods],
+        value=None,
+        placeholder='Select model for forecasting',
+        clearable=False
+    ),
+    dcc.Dropdown(
+        id='forecast-variable-dropdown',
+        options=[{'label': col, 'value': col} for col in df_test.columns],
+        value=None,  # Default value
+        placeholder='Select features for forecasting',
+        multi=True  # Allow multiple selections
+    ),
+    html.Button('Forecast', id='train-model-button', n_clicks=0), # Button to train the model
+    html.Div(id='model-training-output'),  # Placeholder for displaying model training results
+    html.Div(id='metrics-output')  # Container for displaying forecasting metrics
+])
+
+# Callback to display selected forecasting method
+@app.callback(Output('forecasting-results', 'children'),
+              Input('forecasting-method-dropdown', 'value'))
+def display_forecasting_results(method):
+    return html.Div(f'Selected Forecasting Method: {method}')
+
+# Callback for training the model and displaying results
+@app.callback(
+    Output('model-training-output', 'children'),
+    Input('train-model-button', 'n_clicks'),
+    State('forecasting-method-dropdown', 'value'),
+    State('forecast-variable-dropdown', 'value')
+)
+def train_and_display_model(n_clicks, model_name, features):
+    if n_clicks > 0:
+        if features is None or len(features) == 0 or model_name is None or len(model_name) == 0:
+            return html.Div('Please select at least one feature and a model for training the model.')
+        else:
+            model_info, trained_model = helpers.train_model(training_data, model_name, features)
+            predictions = helpers.get_predictions(df_test, trained_model, features)
+            predictions_dict = {'model_info': model_info, 'predictions': predictions.tolist()}  # Convert predictions to a list
+            predictions_json = json.dumps(predictions_dict, indent=2)
+
+            # Plotting
+            trace_actual = go.Scatter(x=df_test.index, y=df['Civil (kWh)'], mode='lines', name='Actual Data')
+            trace_predicted = go.Scatter(x=df_test.index, y=predictions, mode='lines', name='Predicted Data')
+            layout = go.Layout(title='Actual vs Predicted Data', xaxis=dict(title='Index'), yaxis=dict(title='Civil (kWh)'))
+            fig = go.Figure(data=[trace_actual, trace_predicted], layout=layout)
+
+            return html.Div([
+                html.H5(f'{model_name} model using {features} as features trained successfully'),
+                html.Div('Let´s see the predictions!'),
+                dcc.Graph(id='prediction-plot', figure=fig),  # Display the plot
+                html.Div('Let´s evaluate the results!'),
+                html.Div([  # Container for metric dropdown menu
+                    dcc.Dropdown(
+                        id='metric-dropdown',
+                        options=[{'label': metric, 'value': metric} for metric in available_metrics],
+                        value=None,  # Default value
+                        placeholder='Select an evaluation metric',
+                        clearable=False
+                    )
+                ])
+            ])
+
+
+# Forecasting metrics
+# Define a function to calculate metrics
+def calculate_metrics(y_true, y_pred):
+    MAE = metrics.mean_absolute_error(y_true, y_pred)
+    MBE = np.mean(y_true.values - y_pred)
+    MSE = metrics.mean_squared_error(y_true, y_pred)
+    RMSE = np.sqrt(MSE)
+    cvRMSE = RMSE / np.mean(y_true)
+    NMBE = MBE / np.mean(y_true)
+    
+    # Define the metrics dictionary with both abbreviations and long words
+    metrics_dict = {
+        'Mean Absolute Error (MAE)': MAE,
+        'MAE': MAE,
+        'Mean Bias Error (MBE)': MBE,
+        'MBE': MBE,
+        'Mean Squared Error (MSE)': MSE,
+        'MSE': MSE,
+        'Root Mean Squared Error (RMSE)': RMSE,
+        'RMSE': RMSE,
+        'Coefficient of Variation RMSE (cvRMSE)': cvRMSE,
+        'cvRMSE': cvRMSE,
+        'Normalized Mean Bias Error (NMBE)': NMBE,
+        'NMBE': NMBE
+    }
+    
+    return metrics_dict
+
+# Callback to update the selected metric
+@app.callback(
+    Output('metrics-output', 'children'),
+    [Input('metric-dropdown', 'value'),
+     Input('prediction-plot', 'figure')]
+)
+def update_metric(metric, prediction_plot):
+    if not prediction_plot or 'y' not in prediction_plot['data'][1] or metric is None:  # Check if prediction plot is available
+        return ''  # Return empty string if prediction plot is not available
+
+    y_true = df['Civil (kWh)'][1:]  # Get actual values from the DataFrame
+    y_pred = prediction_plot['data'][1]['y']  # Get predicted values from the plot
+
+    calculated_metrics = calculate_metrics(y_true, y_pred)  # Calculate metrics
+    selected_metric_value = calculated_metrics[metric]  # Get the value of the selected metric
+
+    return html.Div(f'{metric}: {selected_metric_value}')  # Display selected metric value
+
+
+# Running the Dash app
+if __name__ == '__main__':
+    app.run_server()
---
+++ Energy_Dashboard_helpers.py
@@ -0,0 +1,268 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from sklearn.neural_network import MLPRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from xgboost import XGBRegressor
+
+
+def load_data():
+    # Load data
+    consumption_filename1 = r"IST_Civil_Pav_2017 - IST_Civil_Pav_2017.csv"
+    consumption_filename2 = r"IST_Civil_Pav_2018 - IST_Civil_Pav_2018.csv"
+    meteo_data_filename = r"IST_meteo_data_2017_2018_2019 - IST_meteo_data_2017_2018_2019.csv"
+    consumption_df1 = pd.read_csv(consumption_filename1)
+    consumption_df2 = pd.read_csv(consumption_filename2)
+    consumption_df = pd.concat([consumption_df1, consumption_df2])
+    meteo_data_df = pd.read_csv(meteo_data_filename)
+
+    # Convert date columns to datetime format with corrected format
+    consumption_df['Date_start'] = pd.to_datetime(consumption_df['Date_start'], format='%d-%m-%Y %H:%M')
+    meteo_data_df['yyyy-mm-dd hh:mm:ss'] = pd.to_datetime(meteo_data_df['yyyy-mm-dd hh:mm:ss'], format='%Y-%m-%d %H:%M:%S')
+
+    # Set the datetime columns as the index
+    consumption_df.set_index('Date_start', inplace=True)
+    meteo_data_df.set_index('yyyy-mm-dd hh:mm:ss', inplace=True)
+
+    # Resample both datasets to minute frequency
+    consumption_df_resampled = consumption_df.resample('min').mean()
+    meteo_data_df_resampled = meteo_data_df.resample('min').mean()
+
+    # Merge the resampled datasets
+    merged_df = pd.merge(consumption_df_resampled, meteo_data_df_resampled, how='outer', left_index=True, right_index=True)
+
+    # Filter rows to include only data from 2017 and 2018
+    merged_df = merged_df.loc['2017-01-01':'2018-12-31']
+
+    # Resample the merged dataset to hourly frequency
+    merged_df_hourly = merged_df.resample('h').mean()
+
+    # Filter rows where Power_kW is not equal to 0
+    merged_df_hourly = merged_df_hourly[merged_df_hourly['Power_kW'] != 0.0]
+
+    # Define the maximum allowed gap size (in hours)
+    max_allowed_gap_hours = 48  # 2 days
+
+    # Calculate the time differences between consecutive timestamps
+    time_diff = meteo_data_df.index.to_series().diff()
+
+    # Find the indices where the time difference exceeds the maximum allowed gap size
+    large_gaps_indices = time_diff[time_diff > pd.Timedelta(hours=max_allowed_gap_hours)].index
+
+    # Define a list to store the labels of rows to be dropped
+    rows_to_drop = []
+
+    # Iterate through the large gaps indices
+    for index in large_gaps_indices:
+        # Find the corresponding labels in merged_df_hourly within the gap
+        labels_within_gap = merged_df_hourly.index[(merged_df_hourly.index >= index - time_diff[index]) &
+                                                    (merged_df_hourly.index <= index)]
+
+        # Extend the rows_to_drop list with the labels within the gap
+        rows_to_drop.extend(labels_within_gap)
+
+    # Drop the rows corresponding to the identified gaps
+    merged_df_hourly = merged_df_hourly.drop(index=rows_to_drop)
+
+    # Create a copy of the original DataFrame to avoid modifying it
+    df_interpolate_linear = merged_df_hourly.copy()
+
+    # Replace NaN values using interpolation
+    df_interpolate_linear = df_interpolate_linear.interpolate(method='linear')
+
+    # Create a new column 'is_weekend' in the DataFrame and set it to 1 for weekend days (Saturday or Sunday)
+    df_interpolate_linear['is_weekend'] = (df_interpolate_linear.index.weekday >= 5).astype(int)
+
+    # Extract features from timestamp
+    df_interpolate_linear['Day_of_Week'] = df_interpolate_linear.index.dayofweek
+    df_interpolate_linear['Date'] = df_interpolate_linear.index.dayofyear
+    df_interpolate_linear['Hour_of_Day'] = df_interpolate_linear.index.hour
+
+    # Perform one-hot encoding for Day of the Week
+    #df_interpolate_linear = pd.get_dummies(df_interpolate_linear, columns=['Day_of_Week'], prefix='Day')
+
+    # Perform cyclical encoding for Hour of the Day
+    df_interpolate_linear['Hour_Sin'] = np.sin(2 * np.pi * df_interpolate_linear['Hour_of_Day'] / 24)
+    df_interpolate_linear['Hour_Cos'] = np.cos(2 * np.pi * df_interpolate_linear['Hour_of_Day'] / 24)
+
+    # Perform cyclical encoding for Day of week
+    df_interpolate_linear['Day_of_Week_Sin'] = np.sin(2 * np.pi * df_interpolate_linear['Day_of_Week'] / 7)
+    df_interpolate_linear['Day_of_Week_Cos'] = np.cos(2 * np.pi * df_interpolate_linear['Day_of_Week'] / 7)
+
+    # Perform cyclical encoding for Date
+    df_interpolate_linear['Date_Sin'] = np.sin(2 * np.pi * df_interpolate_linear['Date'] / 365)
+    df_interpolate_linear['Date_Cos'] = np.cos(2 * np.pi * df_interpolate_linear['Date'] / 365)
+
+    # Drop the original timestamp and Hour_of_Day columns
+    df_interpolate_linear = df_interpolate_linear.drop(columns=['Hour_of_Day', 'Day_of_Week', 'Date'])
+
+    # Add a column with Power_kW of the previous hour
+    df_interpolate_linear['Power_kW_-1'] = df_interpolate_linear['Power_kW'].shift(1)
+
+    # Convert the index to WET timezone
+    df_interpolate_linear.index = df_interpolate_linear.index.tz_localize('Europe/Lisbon', ambiguous='NaT', nonexistent='shift_forward')
+
+    # Convert the index to UTC timezone (DST will be handled automatically)
+    df_interpolate_linear.index = df_interpolate_linear.index.tz_convert('UTC')
+
+    # Sample list of public holidays and academic holidays
+    holiday_list = [
+        '2017-01-01', '2017-02-14', '2017-02-28', '2017-03-19', '2017-03-20', '2017-03-26',
+        '2017-04-14', '2017-04-16', '2017-04-25', '2017-05-01', '2017-05-07', '2017-05-12',
+        '2017-05-12', '2017-06-10', '2017-06-13',
+        '2017-06-15', '2017-06-21', '2017-08-15',
+        '2017-09-22', '2017-10-05',
+        '2017-10-29', '2017-11-01', '2017-12-01', '2017-12-08', '2017-12-21', '2017-12-24',
+        '2017-12-25', '2017-12-31',
+        '2018-01-01', '2018-02-13', '2018-02-14', '2018-03-19', '2018-03-19', '2018-03-20', '2018-03-25',
+        '2018-03-30', '2018-04-01', '2018-04-25', '2018-05-01', '2018-05-06', '2018-05-31', '2018-06-10', '2018-06-13',
+        '2018-06-21', '2018-08-15', '2018-09-23', '2018-10-05', '2018-10-28',
+        '2018-11-01', '2018-12-01', '2018-12-08', '2018-12-21', '2018-12-24', '2018-12-25',
+        '2018-12-31'
+    ]
+
+    # Convert the list of holidays to a pandas DateTimeIndex
+    holiday_index = pd.to_datetime(holiday_list)
+
+    # Create a new column 'is_holiday' in the DataFrame and set it to 1 for holiday days
+    df_interpolate_linear['is_holiday'] = df_interpolate_linear.index.isin(holiday_index).astype(int)
+
+    # Iterate over the holiday dates
+    for holiday_date in holiday_index:
+        # Set the holiday information for all hours of the holiday date
+        df_interpolate_linear.loc[df_interpolate_linear.index.date == holiday_date.date(), 'is_holiday'] = 1
+
+    # Drop NaN value (First entry of Power-1):
+    df_interpolate_linear = df_interpolate_linear.dropna()
+
+    return df_interpolate_linear
+
+def calculate_feature_importance_RF(data):
+    # Split the data into features (X) and target variable (y)
+    X = data.drop(columns=['Power_kW'])
+    y = data['Power_kW']
+
+    # Train the Random Forest model
+    rf_model = RandomForestRegressor(random_state=42)
+    rf_model.fit(X, y)
+
+    # Get feature importances
+    feature_importances = rf_model.feature_importances_
+
+    # Create a DataFrame to display feature importances
+    feature_importance_df = pd.DataFrame({'Feature': X.columns, 'Importance': feature_importances})
+    feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
+
+    return feature_importance_df
+
+def train_model(data, model_name, features):
+    # Split the data into features (X) and target variable (y)
+    X = data[[feature for feature in features if feature in data.columns]]
+
+    # Filter the y data to include only columns that are in the features list
+    y = data['Power_kW']
+
+    # Train the model
+    if model_name == 'Random Forest':
+        model = RandomForestRegressor(random_state=42)
+    elif model_name == 'Linear Regression':
+        model = LinearRegression()
+    elif model_name == 'Support Vector Machine (RBF kernel)':
+        model = SVR()
+    elif model_name == 'Gradient Boosting':
+        model = GradientBoostingRegressor(random_state=42)
+    elif model_name == 'XGBoost':
+        model = XGBRegressor(random_state=42)
+    elif model_name == 'Multi-layer Perceptron':   
+        model = MLPRegressor(random_state=42)
+    elif model_name == 'Decision Tree':
+        model = DecisionTreeRegressor(random_state=42)
+    else:
+        raise ValueError('Invalid model name. Please select a valid model.')
+
+    model.fit(X, y)
+
+    # Define model name based on features and model_name
+    model_name = model_name + ' (' + ', '.join(features) + ')'
+
+    return [model_name, model]
+
+def get_predictions(data, model, features):
+    # Split the data into features (X) and target variable (y)
+    X = data[[feature for feature in features if feature in data.columns]]
+
+    # Get the predictions
+    predictions = model.predict(X)
+
+    return predictions
+
+def create_test_data(dataframe):
+    consumption_df3 = dataframe.copy()
+    # Convert date columns to datetime format with corrected format
+    consumption_df3['Date'] = pd.to_datetime(consumption_df3['Date'], format='%Y-%m-%d %H:%M:%S')
+
+    # Set the datetime columns as the index
+    consumption_df3.set_index('Date', inplace=True)
+
+    # Create a new column 'is_weekend' in the DataFrame and set it to 1 for weekend days (Saturday or Sunday)
+    consumption_df3['is_weekend'] = (consumption_df3.index.weekday >= 5).astype(int)
+
+    # Extract features from timestamp
+    consumption_df3['Day_of_Week'] = consumption_df3.index.dayofweek
+    consumption_df3['Hour_of_Day'] = consumption_df3.index.hour
+    consumption_df3['Date'] = consumption_df3.index.dayofyear
+
+    # Perform cyclical encoding for Hour of the Day
+    consumption_df3['Hour_Cos'] = np.cos(2 * np.pi * consumption_df3['Hour_of_Day'] / 24)
+    consumption_df3['Hour_Sin'] = np.sin(2 * np.pi * consumption_df3['Hour_of_Day'] / 24)
+
+    # Perform cyclical encoding for Day of week
+    consumption_df3['Day_of_Week_Sin'] = np.sin(2 * np.pi * consumption_df3['Day_of_Week'] / 7)
+    consumption_df3['Day_of_Week_Cos'] = np.cos(2 * np.pi * consumption_df3['Day_of_Week'] / 7)
+
+    # Perform cyclical encoding for Date
+    consumption_df3['Date_Sin'] = np.sin(2 * np.pi * consumption_df3['Date'] / 365)
+    consumption_df3['Date_Cos'] = np.cos(2 * np.pi * consumption_df3['Date'] / 365)
+
+    # Drop the original timestamp and Hour_of_Day columns
+    consumption_df3 = consumption_df3.drop(columns=['Hour_of_Day', 'Day_of_Week', 'Date'])
+
+    # Sample list of public holidays and academic holidays
+    holiday_list_2019 = [
+        '2019-01-01', '2019-02-14', '2019-03-05', '2019-03-19', '2019-03-20',
+        '2019-04-19', '2019-04-21', '2019-04-25', '2019-05-01', '2019-05-05', '2019-06-10', '2019-06-13',
+        '2019-06-20', '2019-08-15', '2019-10-05', '2019-11-01', '2019-12-01', '2019-12-08', '2019-12-24',
+        '2019-12-25', '2019-12-31'
+    ]
+
+    # Convert the list of holidays to a pandas DateTimeIndex
+    holiday_index = pd.to_datetime(holiday_list_2019)
+
+    # Create a new column 'is_holiday' in the DataFrame and set it to 1 for holiday days
+    consumption_df3['is_holiday'] = consumption_df3.index.isin(holiday_index).astype(int)
+
+    # Iterate over the holiday dates
+    for holiday_date in holiday_index:
+        # Set the holiday information for all hours of the holiday date
+        consumption_df3.loc[consumption_df3.index.date == holiday_date.date(), 'is_holiday'] = 1
+
+    # Convert the index to WET timezone
+    consumption_df3.index = consumption_df3.index.tz_localize('Europe/Lisbon', ambiguous='NaT', nonexistent='shift_forward')
+
+    # Convert the index to UTC timezone (DST will be handled automatically)
+    consumption_df3.index = consumption_df3.index.tz_convert('UTC')
+
+    # Add a column with Power_kW of the previous hour
+    consumption_df3['Power_kW'] = consumption_df3['Civil (kWh)']
+    consumption_df3 = consumption_df3.drop(columns=['Civil (kWh)'])
+    consumption_df3['Power_kW_-1'] = consumption_df3['Power_kW'].shift(1)
+
+    consumption_df3 = consumption_df3.drop(columns=['Power_kW'])
+
+    # Drop NaN value (First entry of Power-1):
+    consumption_df3 = consumption_df3.dropna()
+
+    return consumption_df3
